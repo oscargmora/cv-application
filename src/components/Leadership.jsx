eb82c149f0996ad16f8a85f54fba08fc9d80158b
Oscar Mora <oscgmora@gmail.com>
2024-01-20 20:39:05 -0500

    Leadership Section

--- src/components/Leadership.jsx
+++ src/components/Leadership.jsx
@@ -26,8 +26,8 @@ function LeadershipSection({leadership, handleChange}) {
                 ))
             }
             <Input label="Location"  id="location" handleChange={handleChange} />
-            <Input label="Start Date"  id="start-date" handleChange={handleChange} />
-            <Input label="End Date"  id="end-date" handleChange={handleChange} />
+            <Input label="Start Date"  id="startDate" handleChange={handleChange} />
+            <Input label="End Date"  id="endDate" handleChange={handleChange} />
             <Input label="Title"  id="title" handleChange={handleChange} />
             <Input label="City"  id="city" handleChange={handleChange} />
             <Input label="Description"  id="description" handleChange={handleChange} />
@@ -35,23 +35,41 @@ function LeadershipSection({leadership, handleChange}) {
     )
 }
 
+LeadershipSection.propTypes = {
+    leadership: PropTypes.object.isRequired,
+    handleChange: PropTypes.func.isRequired
+}
+
 function Leadership() {
     const [leadership, setLeadership] = useState([
         {location: 'Student Ambassadors at Broward College', startDate: 'Sep 2017', endDate:'Jul 2018', title: 'Orientation Leader', city: 'Coconut Creek, FL', description: 'A volunteer program that allowed students in the organization to plan and run events for the rest of the students at Broward College. Served to also show students what classes they should take as a freshman if promoted.'}
     ])
 
-    const handleChange = (e) => {
-        console.log(e.target.value)
+    const handleChange = (e, index) => {
+        setLeadership(prevLeadership => {
+            const newLeadership = [...prevLeadership];
+            newLeadership[index][e.target.id] = e.target.value;
+            return newLeadership;
+        })
+    }
+
+    const addLeadershipPosition = () => {
+        setLeadership(prevLeadership => {
+            const newLeadership = [...prevLeadership];
+            const newLeadershipPosition = {location: 'Student Ambassadors at Broward College', startDate: 'Sep 2017', endDate:'Jul 2018', title: 'Orientation Leader', city: 'Coconut Creek, FL', description: 'A volunteer program that allowed students in the organization to plan and run events for the rest of the students at Broward College. Served to also show students what classes they should take as a freshman if promoted.'};
+            newLeadership.push(newLeadershipPosition);
+            return newLeadership;
+        })
     }
 
     return (
         <div className="leadership">
             {
                 leadership.map((leadership, index) => (
-                    <LeadershipSection key={`leadership-section-${index}`} leadership={leadership} handleChange={(e) => handleChange(e)} />
+                    <LeadershipSection key={`leadership-section-${index}`} leadership={leadership} handleChange={(e) => handleChange(e, index)} />
                 ))
             }
-            
+            <button onClick={addLeadershipPosition}>Add Leadership Position</button>
         </div>
     )
 }
